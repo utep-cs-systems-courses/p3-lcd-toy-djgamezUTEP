c9f822baee42665a9d8e1ef6f39a3c895ade483a
student <djgamez@miners.utep.edu>
2021-11-19 09:45:36 -0700

    added outline to BTC and made different shift moves with each button

--- project/switches.c
+++ project/switches.c
@@ -1,11 +1,16 @@
 #include <msp430.h>
 #include "switches.h"
 #include "led.h"
-
+#include "lcdutils.h"
+#include "lcddraw.h"
+#include "draw_shapes.h"
 extern char redraw_screen;
 const char down = 1;
 const char up = 0;
-
+u_int green= COLOR_GREEN;
+u_int purple= COLOR_PURPLE;
+u_int blue= COLOR_BLUE;
+u_int grey= COLOR_GRAY;
 char switch1_state, switch2_state, switch3_state, switch4_state, switch_state_changed;
 
 /* update he interrupt sense bits so 
@@ -46,16 +51,28 @@ check_p2_switches(void)
   switch3_state = (p2val & SW3) ? 0 : 1;
   switch4_state = (p2val & SW4) ? 0 : 1;
   if(switch1_state==1){
-    xShift+=2;
+    xShift+=8;
+    yShift+=8;
+    draw_BTC(xShift, yShift);
+    clearScreen(green);
   }
   if(switch2_state==1){
-    xShift-=2;
+    xShift-=8;
+    yShift-=8;
+    draw_BTC(xShift, yShift);
+    clearScreen(purple);
   }
   if(switch3_state==1){
-    yShift+=2;
+    yShift+=4;
+    xShift-=7;
+    draw_BTC(xShift, yShift);
+    clearScreen(blue);
   }
   if(switch4_state==1){
-    yShift-=2;
+    yShift-=5;
+    xShift+=10;
+    draw_BTC(xShift, yShift);
+    clearScreen(grey);
   }
   redraw_screen = 1;            // switch status changed, redraw
 }
